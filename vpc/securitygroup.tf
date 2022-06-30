e334b800bc7c0139cf349ce1ebf062c0bedd60d0
shockliang <shockwater@gmail.com>
2022-06-30 14:39:17 +0800

    Provisioning instances in the vpc.

---
+++ vpc/securitygroup.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "allow-ssh" {
+  vpc_id      = aws_vpc.main.id
+  name        = "allow-ssh"
+  description = "security group that allow ssh and all egress traffic"
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port  = 22
+    to_port    = 22
+    protocol   = "tcp"
+  }
+  tags = {
+    Name = "allow-ssh"
+  }
+}
