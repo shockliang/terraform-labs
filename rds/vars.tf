4c70bd3ad5332e217781779774078bc432fb7884
shockliang <shockwater@gmail.com>
2022-07-02 13:16:44 +0800

    RDS lab.

---
+++ rds/vars.tf
@@ -0,0 +1,25 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0f65ab0fd913bc7be"
+    us-west-1 = "ami-08daca4640726cc73"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "RDS_PASSWORD" {
+}
+
