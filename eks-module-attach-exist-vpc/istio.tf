3c58f98a6367684c0199571540ada7217616530a
shockliang <shockwater@gmail.com>
2022-08-02 17:05:22 +0800

    Extracting providers.

--- eks-module-attach-exist-vpc/istio.tf
+++ eks-module-attach-exist-vpc/istio.tf
@@ -1,17 +1,3 @@
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
-    }
-  }
-}
-
 locals {
   istio_charts_url = "https://istio-release.storage.googleapis.com/charts"
 }
