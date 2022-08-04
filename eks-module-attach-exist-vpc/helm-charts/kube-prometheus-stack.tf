33a471a5a5b50739ddebc5e440b595e86a512ec0
shockliang <shockwater@gmail.com>
2022-08-04 12:36:49 +0800

    Install kube prometheus stack.

---
+++ eks-module-attach-exist-vpc/helm-charts/kube-prometheus-stack.tf
@@ -0,0 +1,19 @@
+locals {
+  kube_prometheus_stack_charts_url = "https://prometheus-community.github.io/helm-charts"
+  kube_prometheus_stack_version = "39.2.1"
+}
+
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+resource "helm_release" "kube-prometheus-statck" {
+  repository      = local.kube_prometheus_stack_charts_url
+  chart           = "kube-prometheus-stack"
+  name            = "monitoring"
+  version         = local.kube_prometheus_stack_version
+  namespace       = kubernetes_namespace.monitoring.id
+  cleanup_on_fail = true
+}
