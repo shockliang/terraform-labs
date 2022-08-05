2d82d8622e256926377cc51a15af198598329779
shockliang <shockwater@gmail.com>
2022-08-05 10:53:17 +0800

    Install metrics server helm charts for HPA of eks.

---
+++ eks-module-attach-exist-vpc/metrics-server.tf
@@ -0,0 +1,12 @@
+locals {
+  metrics_server_chart_url = "https://kubernetes-sigs.github.io/metrics-server"
+  metrics_server_chart_version = "3.8.2"
+}
+
+resource "helm_release" "metrics_server" {
+  repository      = local.metrics_server_chart_url
+  chart           = "metrics-server"
+  name            = "metrics-server"
+  version         = local.metrics_server_chart_version
+  cleanup_on_fail = true
+}
