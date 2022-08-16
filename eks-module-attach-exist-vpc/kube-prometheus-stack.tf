c1fbf75b2451b09a0fd3fae5a114a82d3668b117
shockliang <shockwater@gmail.com>
2022-08-16 21:18:29 +0800

    Adding grafana persistence settings.

--- eks-module-attach-exist-vpc/kube-prometheus-stack.tf
+++ eks-module-attach-exist-vpc/kube-prometheus-stack.tf
@@ -1,12 +1,15 @@
 locals {
   kube_prometheus_stack_charts_url = "https://prometheus-community.github.io/helm-charts"
-  kube_prometheus_stack_version = "39.2.1"
+  kube_prometheus_stack_version = "39.7.0"
 }
 
 resource "kubernetes_namespace" "monitoring" {
   metadata {
     name = "monitoring"
   }
+  depends_on = [
+    helm_release.metrics_server
+  ]
 }
 
 resource "helm_release" "kube-prometheus-statck" {
@@ -15,6 +18,6 @@ resource "helm_release" "kube-prometheus-statck" {
   name            = "monitoring"
   version         = local.kube_prometheus_stack_version
   namespace       = kubernetes_namespace.monitoring.id
-  values = [ "${file("prometheus.yaml")}" ]
+  values = [ "${file("kube-prometheus-stack-values.yaml")}"]
   cleanup_on_fail = true
 }
