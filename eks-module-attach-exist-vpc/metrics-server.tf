0d24650ed7a4e6f2fc7ebb106864f64f195e8436
shockliang <shockwater@gmail.com>
2022-09-01 11:44:54 +0800

    Moving metrics server into monitoring namespace.

--- eks-module-attach-exist-vpc/metrics-server.tf
+++ eks-module-attach-exist-vpc/metrics-server.tf
@@ -1,5 +1,5 @@
 locals {
-  metrics_server_chart_url = "https://kubernetes-sigs.github.io/metrics-server"
+  metrics_server_chart_url     = "https://kubernetes-sigs.github.io/metrics-server"
   metrics_server_chart_version = "3.8.2"
 }
 
@@ -8,5 +8,6 @@ resource "helm_release" "metrics_server" {
   chart           = "metrics-server"
   name            = "metrics-server"
   version         = local.metrics_server_chart_version
+  namespace       = kubernetes_namespace.monitoring.id
   cleanup_on_fail = true
 }
