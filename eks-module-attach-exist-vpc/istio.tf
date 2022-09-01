d3162295ea070cbb83d11067c1d4dd0731c1659d
shockliang <shockwater@gmail.com>
2022-09-01 11:47:00 +0800

    Marking istio ingress gateway using nlb and single az.

--- eks-module-attach-exist-vpc/istio.tf
+++ eks-module-attach-exist-vpc/istio.tf
@@ -22,23 +22,26 @@ resource "helm_release" "istio-base" {
 }
 
 resource "helm_release" "istiod" {
-  repository      = local.istio_charts_url
-  chart           = "istiod"
-  name            = "istiod"
-  version         = var.istio_version
-  namespace       = kubernetes_namespace.istio-system.id
+  repository = local.istio_charts_url
+  chart      = "istiod"
+  name       = "istiod"
+  version    = var.istio_version
+  namespace  = kubernetes_namespace.istio-system.id
   # timeout         = 120
   cleanup_on_fail = true
-  depends_on = [helm_release.istio-base]
+  depends_on      = [helm_release.istio-base]
 }
 
-resource "helm_release" "istio-ingress" {
-  repository      = local.istio_charts_url
-  chart           = "gateway"
-  name            = "istio-ingress"
-  namespace       = kubernetes_namespace.istio-system.id
-  version         = var.istio_version
+resource "helm_release" "istio-ingressgateway" {
+  repository = local.istio_charts_url
+  chart      = "gateway"
+  name       = "istio-ingressgateway"
+  namespace  = kubernetes_namespace.istio-system.id
+  version    = var.istio_version
+  values          = [
+    templatefile("istio-values.tftpl", {primary-subnet-id = "${aws_subnet.eks-node-primary-subnet.id}"})
+  ]
   # timeout         = 500
   cleanup_on_fail = true
-  depends_on = [helm_release.istiod]
+  depends_on      = [helm_release.istiod]
 }
